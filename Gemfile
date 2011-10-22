4efac4d5a03ae16a0f1aa04b9c3754a7fa1bbfa5
Ryan Fitzgerald <ryan@codebrewstudios.com>
2011-10-22 01:34:35 -0400

    setting up user model

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'rails', '3.1.1'
 # gem 'rails',     :git => 'git://github.com/rails/rails.git'
 
 gem 'sqlite3'
+gem "devise", "~> 1.4.9"
 
 # Gems used only for assets and not required
 # in production environments by default.
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 BackboneAdmin::Application.routes.draw do
+  devise_for :users do
+    get "/logout" => "devise/sessions#destroy"
+  end
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
